c97755fba781ae3be649218dbf3a9b88e98fea50
renmu123 <1101022351@qq.com>
2023-10-16 18:23:16 +0800

    update

--- src/server/api.py
+++ src/server/api.py
@@ -13,11 +13,23 @@ def list_parser():
 
 
 class Job(BaseModel):
-    name: str
+    site: str
     goods_id: str
 
 
 @app.post("/job/add")
 def add_job(job: Job):
-    parser = parserFactory(job.name)
-    scheduler.add_job(parser.parse, "interval", minutes=1, args=[job.goods_id])
+    parser = parserFactory(job.site)
+    job = scheduler.add_job(parser.parse, "cron", minute="1", hour="0", day="*/1", args=[job.goods_id])
+    return {"id": job.id}
+
+
+@app.get("/job/list")
+def list_job():
+    return scheduler.get_jobs()
+
+
+@app.post("/job/remove")
+def remove_job(job_id: str):
+    scheduler.remove_job(job_id)
+    return {"id": job_id}
